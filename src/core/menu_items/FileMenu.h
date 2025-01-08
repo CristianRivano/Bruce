fee54f92cb18c5aea576042fdc62f07774c422ee
Ka0s <Rivano@gmail.com>
2025-01-07 23:30:29 -0300

    Actualizar FileMenu.h

--- src/core/menu_items/FileMenu.h
+++ src/core/menu_items/FileMenu.h
@@ -6,7 +6,7 @@
 
 class FileMenu : public MenuItemInterface {
 public:
-    FileMenu() : MenuItemInterface("Files") {}
+    FileMenu() : MenuItemInterface("Archivos") {}
 
     void optionsMenu(void);
     void drawIcon(float scale);
