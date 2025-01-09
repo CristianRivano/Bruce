19827710f2e07ae859e01f5022c3a8bcb1c7704f
Ka0s <Rivano@gmail.com>
2025-01-08 21:52:07 -0300

    Actualizar WifiMenu.cpp

--- src/core/menu_items/WifiMenu.cpp
+++ src/core/menu_items/WifiMenu.cpp
@@ -31,7 +31,7 @@
 void WifiMenu::optionsMenu() {
     if(!wifiConnected) {
         options = {
-            {"Conectar Wifi", [=]()  { wifiConnectMenu(WIFI_STA); }},
+            {"Conectar WiFi", [=]()  { wifiConnectMenu(WIFI_STA); }},
             {"WiFi AP",      [=]()  { wifiConnectMenu(WIFI_AP); displayInfo("pwd: " + bruceConfig.wifiAp.pwd, true); }},
         };
     } else {
@@ -39,15 +39,15 @@ void WifiMenu::optionsMenu() {
         if(WiFi.getMode() == WIFI_MODE_STA) options.push_back({"AP info",   [=]()  { displayAPInfo(); }});
     }
     options.push_back({"Ataque WiFi", [=]()     { wifi_atk_menu(); }});
-    options.push_back({"Evil Portal", [=]()   { EvilPortal(); }});
+    options.push_back({"Portal Cautivo", [=]()   { EvilPortal(); }});
     options.push_back({"ReverseShell", [=]()       { ReverseShell(); }});
-    options.push_back({"Listen TCP", [=]()       { listenTcpPort(); }});
+    options.push_back({"Escuchar TCP", [=]()       { listenTcpPort(); }});
 #ifndef LITE_VERSION
     options.push_back({"TelNET", [=]()        { telnet_setup(); }});
     options.push_back({"SSH", [=]()           { ssh_setup(); }});
     options.push_back({"DPWO", [=]()          { dpwo_setup(); }});
-    options.push_back({"Raw Sniffer", [=]()   { sniffer_setup(); }});
-    options.push_back({"Scan Hosts", [=]()    { local_scan_setup(); }});
+    options.push_back({"RAW Sniffer", [=]()   { sniffer_setup(); }});
+    options.push_back({"Escanear hosts", [=]()    { local_scan_setup(); }});
     options.push_back({"Wireguard", [=]()     { wg_setup(); }});
     options.push_back({"Brucegotchi",  [=]()   { brucegotchi_start(); }});
 #endif
